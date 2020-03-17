d2524fea61543030158cddc3251f6bd07a7f21ef
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-17 22:04:17 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,30 @@
 class Show < ActiveRecord::Base 
 
-  def highest_rating
-    Show.maximum("rating")
+  def self.highest_rating
+    self.maximum("rating")
   end 
 
-  def most_popular_show
+  def self.most_popular_show
     self.where(rating: self.highest_rating)
   end 
   
-  def lowest_rating
+  def self.lowest_rating
     self.minimum("rating")
   end 
   
-  def least_popular_show
+  def self.least_popular_show
     self.where(rating: self.lowest_rating)
   end 
   
-  def ratings_sum
+  def self.ratings_sum
     self.sum("rating")
   end 
   
-  def popular_shows
+  def self.popular_shows
     self.where("rating" > 5)
   end 
   
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
     self.order(name: :asc)
   end 
   
